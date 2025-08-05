dda2c7e3710c231cfa18b842dba74dd10e0a5574
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-05 22:17:42 +0600

    called insertionSort() and printArray() function for sort an array.

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -21,9 +21,9 @@ void printArray(vector<int>arr){
     }
 }
 
-// int main(){
-//     vector<int>arr= {5,1,0,7,3,4,8,3};
-//     insertionSort(arr);
-//     printArray(arr);
-//     return 0;
-// }
+int main(){
+    vector<int>arr= {5,1,0,7,3,4,8,3};
+    insertionSort(arr);
+    printArray(arr);
+    return 0;
+}
---
+++ Sorting/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+// int main(){
+//     vector<int>arr= {5,1,0,7,3,4,8,3};
+//     insertionSort(arr);
+//     printArray(arr);
+//     return 0;
+// }
