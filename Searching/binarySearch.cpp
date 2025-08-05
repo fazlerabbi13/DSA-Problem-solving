714dd8348f02192f24df3385546180a8e36b9d03
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-05 19:30:55 +0600

    create a function for finding the index of element.

---
+++ Searching/binarySearch.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int> &arr,int x){
+    int low = 0;
+    int high = arr.size()-1;
+
+    while (low<=high)
+    {
+        int mid = low+(high-low)/2;
+
+        if(arr[mid]==x){
+            return mid;
+        }
+        if(arr[mid]<x){
+            low= mid+1;
+        }
+
+        else{
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+    
+}
+
+// int main(){
+
+//     vector<int> arr= {2,3,4,5,10,40};
+//     int x=10;
+
+//     int result = binarySearch(arr,x);
+
+//     if(result == -1){
+//         cout<<"Element not found in array";
+//     }
+//     else{
+//         cout<<"Element found at index "<< result;
+//     }
+
+//     return 0;
+// }
