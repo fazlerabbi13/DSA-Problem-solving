1527c59f070b78d069c1649915ebf4eaede290e8
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-05 20:26:41 +0600

    called bubble sort and print function on main function.

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr){
+    int n = arr.size();
+    bool swapped;
+
+    for(int i=0; i<n-1;i++){
+        swapped = false;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped){
+            break;
+        }
+    }
+}
+
+void printVector(const vector<int>&arr){
+    for(int value: arr){
+        cout<<" "<< value;
+    }
+}
+
+
+int main(){
+    vector<int>arr= {20,10,45,12,9,50,3};
+
+    bubbleSort(arr);
+    printVector(arr);
+
+    return 0;
+}
