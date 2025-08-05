748a4625a0dbbf5d968e1087078b489363f9097e
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-05 22:15:49 +0600

    create printArray() method for printing sorted array.

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -15,7 +15,15 @@ void insertionSort(vector<int>& arr){
     }
 }
 
-int main(){
-
-    return 0;
+void printArray(vector<int>arr){
+    for(int value:arr){
+        cout<<value<<" ";
+    }
 }
+
+// int main(){
+//     vector<int>arr= {5,1,0,7,3,4,8,3};
+//     insertionSort(arr);
+//     printArray(arr);
+//     return 0;
+// }
